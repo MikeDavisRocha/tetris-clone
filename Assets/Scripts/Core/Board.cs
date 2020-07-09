72f0831ced3cfd4abf3b1b02a2ae1e9b82507fff
Mike Davis <mikedavisrocha@gmail.com>
2020-07-09 11:49:07 -0300

    Added height, width of board, transform of square, DrawEmptyCells method.

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField] Transform m_emptySprite;
+    [SerializeField] int m_height = 30;
+    [SerializeField] int m_width = 30;
+
     // Start is called before the first frame update
     void Start()
+    {
+        DrawEmptyCells();
+    }
+
+    private void DrawEmptyCells()
     {
         
     }
@@ -15,4 +25,6 @@ public class Board : MonoBehaviour
     {
         
     }
+
+
 }
